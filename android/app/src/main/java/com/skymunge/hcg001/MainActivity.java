30785b322fa09feb8264eff2c7862bf73967a4c1
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2019-12-29 12:15:28 +1000

    upd

--- android/app/src/main/java/com/skymunge/hcg001/MainActivity.java
+++ android/app/src/main/java/com/skymunge/hcg001/MainActivity.java
@@ -1,9 +1,34 @@
 package com.skymunge.hcg001;
 
 import android.app.NativeActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class MainActivity extends NativeActivity {
     static {
         System.loadLibrary("fmod");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        /*Dialog dialog = new AlertDialog.Builder(this)
+                .setMessage("awdawd")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                }).create();
+
+        dialog.show();*/
     }
 }
