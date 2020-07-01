b47a673ecda00813e440766f1b385f1334a46b1d
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-07-01 18:16:22 +1000

    upd

---
+++ ios/app_delegate.mm
@@ -0,0 +1,52 @@
+#import <platform/system_ios.h>
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+@interface AppDelegate : SkyDelegate
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+//    [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    return [super application: application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    //
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    //
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    //
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+//    [FBSDKAppEvents activateApp];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    //
+}
+
+/*- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation];
+}*/
+
+@end
