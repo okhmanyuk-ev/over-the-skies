f58f6768815b6c824b284b08f40fbca2771fffe7
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-07-05 23:17:41 +1000

    upd ios

--- ios/app_delegate.mm
+++ ios/app_delegate.mm
@@ -39,14 +39,4 @@
     //
 }
 
-/*- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation {
-  return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                        openURL:url
-                                              sourceApplication:sourceApplication
-                                                     annotation:annotation];
-}*/
-
 @end
