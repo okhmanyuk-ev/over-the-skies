db607fb0ed03ea9d4af75f5f4d5c3c48a6c0536b
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-01-14 10:39:02 +1000

    remove udp networking, start websocket networking

--- src/client.h
+++ src/client.h
@@ -6,37 +6,9 @@
 
 namespace hcg001
 {
-	class Channel : public Shared::Networking::Channel
-	{
-	public:
-		Channel();
-
-	public:
-		void sendEvent(const std::string& name, const std::map<std::string, std::string>& params);
-
-	private:
-		std::map<std::string, std::function<void(std::map<std::string, std::string>)>> mEvents;
-        
-        struct FileDef
-        {
-            size_t progress = 0;
-            Common::BitBuffer buf;
-        };
-		std::map<std::string, FileDef> mFiles;
-	};
-
 	class Client : public Shared::Networking::Client
 	{
 	public:
-		Client();
-
-	protected:
-		std::shared_ptr<Shared::Networking::Channel> createChannel() override;
-
-	public:
 		void sendEvent(const std::string& name, const std::map<std::string, std::string>& params);
-
-	private:
-		auto getMyChannel() const { return std::dynamic_pointer_cast<Channel>(getChannel()); }
 	};
 }
