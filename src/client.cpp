ec8c6d92ae9b34e6f5817f232a2f83caeea72c5b
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-01-14 18:18:48 +1000

    restore udp networking

--- src/client.cpp
+++ src/client.cpp
@@ -5,7 +5,103 @@ using namespace hcg001;
 
 // channel
 
+Channel::Channel()
+{
+	addMessageReader("event", [this](auto& buf) {
+		auto name = Common::BufferHelpers::ReadString(buf);
+		auto params = std::map<std::string, std::string>();
+		while (buf.readBit())
+		{
+			auto key = Common::BufferHelpers::ReadString(buf);
+			auto value = Common::BufferHelpers::ReadString(buf);
+			params.insert({ key, value });
+		}
+		if (mEvents.count(name) == 0)
+		{
+			LOG("unknown event: " + name);
+			return;
+		}
+		mEvents.at(name)(params);
+	});
+	addMessageReader("file", [this](Common::BitBuffer& buf) {
+		auto path = Common::BufferHelpers::ReadString(buf);
+		auto file_size = buf.readBitsVar();
+		auto frag_offset = buf.readBitsVar();
+		auto frag_size = buf.readBitsVar();
+
+		if (mFiles.count(path) == 0)
+		{
+			mFiles[path].buf.setSize(file_size);
+		}
+
+		auto& progress = mFiles[path].progress;
+		auto& file_buf = mFiles[path].buf;
+
+		if (file_buf.getSize() != file_size)
+		{
+			LOG("file: bad file size");
+			return;
+		}
+
+		if (frag_offset + frag_size > file_buf.getSize())
+		{
+			LOG("file: bad fragment");
+			return;
+		}
+
+		for (uint32_t i = frag_offset; i < frag_offset + frag_size; i++)
+		{
+			((uint8_t*)file_buf.getMemory())[i] = buf.read<uint8_t>();
+		}
+
+		progress += frag_size;
+
+		STATS_INDICATE_GROUP("net", path, Common::Helpers::BytesToNiceString(progress) + "/" + Common::Helpers::BytesToNiceString(file_size));
+
+		if (progress == file_size)
+		{
+			Platform::Asset::Write(path, file_buf.getMemory(), file_buf.getSize(), Platform::Asset::Storage::Bundle);
+			mFiles.erase(path);
+			LOG(path + " saved");
+		}
+	});
+
+	mEvents["print"] = [](const auto& params) {
+		auto text = params.at("text");
+		EVENT->emit(Helpers::PrintEvent({ text }));
+	};
+}
+
+void Channel::sendEvent(const std::string& name, const std::map<std::string, std::string>& params)
+{
+	auto buf = Common::BitBuffer();
+	Common::BufferHelpers::WriteString(buf, name);
+	for (auto& [key, value] : params)
+	{
+		buf.writeBit(true);
+		Common::BufferHelpers::WriteString(buf, key);
+		Common::BufferHelpers::WriteString(buf, value);
+	}
+	buf.writeBit(false);
+	sendReliable("event", buf);
+}
+
+// client
+
+Client::Client() : Shared::NetworkingUDP::Client({ "192.168.0.106:1337" })
+{
+	//
+}
+
+std::shared_ptr<Shared::NetworkingUDP::Channel> Client::createChannel()
+{
+	return std::make_shared<hcg001::Channel>();
+}
+
 void Client::sendEvent(const std::string& name, const std::map<std::string, std::string>& params)
 {
-	// nothing
+	if (!isConnected())
+		return;
+
+	getMyChannel()->sendEvent(name, params);
 }
--- src/client.h
+++ src/client.h
@@ -6,9 +6,37 @@
 
 namespace hcg001
 {
-	class Client : public Shared::Networking::Client
+	class Channel : public Shared::NetworkingUDP::Channel
 	{
 	public:
+		Channel();
+
+	public:
 		void sendEvent(const std::string& name, const std::map<std::string, std::string>& params);
+
+	private:
+		std::map<std::string, std::function<void(std::map<std::string, std::string>)>> mEvents;
+
+		struct FileDef
+		{
+			size_t progress = 0;
+			Common::BitBuffer buf;
+		};
+		std::map<std::string, FileDef> mFiles;
+	};
+
+	class Client : public Shared::NetworkingUDP::Client
+	{
+	public:
+		Client();
+
+	protected:
+		std::shared_ptr<Shared::NetworkingUDP::Channel> createChannel() override;
+
+	public:
+		void sendEvent(const std::string& name, const std::map<std::string, std::string>& params);
+
+	private:
+		auto getMyChannel() const { return std::dynamic_pointer_cast<Channel>(getChannel()); }
 	};
 }
