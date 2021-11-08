52f83767db6e7ef72532aa9787ae81cd7eff16ea
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-11-08 22:54:21 +1000

    upd

--- src/screen.h
+++ src/screen.h
@@ -4,30 +4,15 @@
 
 namespace hcg001
 {
-	class Screen : public Scene::RenderLayer<Shared::SceneManager::Screen>
+	class Screen : public Shared::SceneHelpers::StandardScreen
 	{
 	public:
 		Screen();
 
-	protected:
-		void onEnterBegin() override;
-		void onEnterEnd() override;
-		void onLeaveBegin() override;
-		void onLeaveEnd() override;
-		void onWindowAppearing() override;
-		void onWindowDisappearing() override;
-
-	protected:
-		std::unique_ptr<Actions::Action> createEnterAction() override;
-		std::unique_ptr<Actions::Action> createLeaveAction() override;
-
 	public:
-		auto getContent() { return mContent; }
 		auto getGui() { return mGui; }
 
 	private:
-		std::shared_ptr<Scene::Node> mContent;
 		std::shared_ptr<Shared::SceneHelpers::SafeArea> mGui;
-		std::shared_ptr<Scene::BlurredGlass> mBlur;
 	};
 }
